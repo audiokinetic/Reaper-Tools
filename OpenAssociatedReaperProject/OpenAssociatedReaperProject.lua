05a38d99ca2005dbd8c09622b130fbc6c535bdf3
Pascal Maheux <pmaheux@audiokinetic.com>
2023-07-24 09:23:10 -0400

    OpenAssociatedReaperProject 1.0.0
    Change-Id: I4910a45699952674957f6b784d44e38ba1ecb6b7

--- OpenAssociatedReaperProject/OpenAssociatedReaperProject.lua
+++ OpenAssociatedReaperProject/OpenAssociatedReaperProject.lua
@@ -1,6 +1,6 @@
 @description Open Associated REAPER Project
 @author Audiokinetic
-@version 1.0.0-rc.2
+@version 1.0.0
 @metapackage
 @provides
   [nomain] Utilities.lua https://raw.githubusercontent.com/audiokinetic/Reaper-Tools/$commit/OpenAssociatedReaperProject/Utilities.lua
